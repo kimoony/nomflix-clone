df01ac4c817a33233e3add31c18263a8932be77a
kimoony <hoonby9011@gmail.com>
2022-04-16 14:00:28 +0900

    Created nomflix-clone file

---
+++ src/styled.d.ts
@@ -0,0 +1,18 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend theme!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    red: string;
+    black: {
+      veryDark: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      darker: string;
+      lighter: string;
+    };
+  }
+}
