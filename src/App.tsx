4d7d9d5c773ffd48996be176be35c1e48feec51c
kimoony <hoonby9011@gmail.com>
2022-04-18 03:58:32 +0900

    nomflix page Header 부분 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
 
 function App() {
   return (
-    <div>
-      null
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="tv" element={<Tv />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div style={{ height: "200vh" }}>
+
+    </div>
+  )
+}
+
+export default Home
